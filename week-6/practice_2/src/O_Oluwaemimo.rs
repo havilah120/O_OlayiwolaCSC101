e6f73df117716c4c7d4fa951f72e0c271cdb2b5e
havilah120 <wemimoy2k@gmail.com>
2023-02-02 23:19:49 -0600

    (my week 6 commit)

---
+++ week-6/practice_2/src/O_Oluwaemimo.rs
@@ -0,0 +1,21 @@
+use std
+fn main() {
+    let mut input = String::new();
+    println!("Input your qualification code");
+    io::stdin().read_line(&mut input).expect("Failed to read input");
+    let input:integer = input.trim().parse().expect("Invalid input");
+
+    code_7();
+}
+
+fn code_7() {
+if input = 7
+    {
+        println!("Input your name",input);
+    }
+    else if 
+    {
+        println!("Character '{}' is not a digit",ch);
+    }
+    println!("Another function.");
+}
---
+++ week-6/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\12149\OneDrive\Desktop\O_OlayiwolaCSC101\week-6\practice_2\target\debug\practice_2.exe: C:\Users\12149\OneDrive\Desktop\O_OlayiwolaCSC101\week-6\practice_2\src\main.rs
